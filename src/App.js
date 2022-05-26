792fca0cf2861ef5b0a7a13b3982f8a75e247ca2
mshyam25 <shyammanikandan.22@gmail.com>
2022-05-26 17:34:30 +0530

    Added Skills Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './Components/Header/Header'
 import IntroSection from './Components/Intro-Section/IntroSection'
+import SkillsSection from './Components/Skills-Section/SkillsSection'
 function App() {
   return (
     <div className='main'>
       <Header />
       <IntroSection />
+      <SkillsSection />
       <h1>Portfolio</h1>
     </div>
   )
---
+++ src/Components/Skills-Section/SkillsSection.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import './Skills.styles.css'
+const SkillsSection = () => {
+  return (
+    <>
+      <div className='skills-section' id='skills'>
+        <h2 className='section-heading'>Skills and Tools</h2>
+
+        <div className='skills'>
+          <div className='skill-box'>
+            <img
+              src='img/skills/react.svg'
+              className='skill-icon'
+              alt='React icon'
+            />
+            <p className='skill-text'>React</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/redux.svg'
+              className='skill-icon'
+              alt='Redux icon'
+            />
+            <p className='skill-text'>Redux</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/nodejs.svg'
+              className='skill-icon'
+              alt='Nodejs icon'
+            />
+            <p className='skill-text'>Node js</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/mongodb.svg'
+              className='skill-icon'
+              alt='Mongodb icon'
+            />
+            <p className='skill-text'>Mongodb</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/js.svg'
+              className='skill-icon'
+              alt='Javascript icon'
+            />
+            <p className='skill-text'>Javascript</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/git.svg'
+              className='skill-icon'
+              alt='Git icon'
+            />
+            <p className='skill-text'>Git</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/css3.svg'
+              className='skill-icon'
+              alt='CSS icon'
+            />
+            <p className='skill-text'>CSS</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/html5.svg'
+              className='skill-icon'
+              alt='Html icon'
+            />
+            <p className='skill-text'>Html</p>
+          </div>
+          <div className='skill-box'>
+            <img
+              src='img/skills/vscode.svg'
+              className='skill-icon'
+              alt='vsCode icon'
+            />
+            <p className='skill-text'>VS Code</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SkillsSection
