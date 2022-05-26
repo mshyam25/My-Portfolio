8e1a79154b6032c4d26438b8a7660af0037aa724
mshyam25 <shyammanikandan.22@gmail.com>
2022-05-26 22:42:42 +0530

    Added Work Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import IntroSection from './Components/Intro-Section/IntroSection'
 import ProjectsSection from './Components/Projects-Section/ProjectsSection'
 import SkillsSection from './Components/Skills-Section/SkillsSection'
 import AboutSection from './Components/About-Section/AboutSection'
+import WorkSection from './Components/Work-Section/WorkSection'
 function App() {
   return (
     <div className='main'>
@@ -11,6 +12,7 @@ function App() {
       <SkillsSection />
       <ProjectsSection />
       <AboutSection />
+      <WorkSection />
       <h1>Portfolio</h1>
     </div>
   )
