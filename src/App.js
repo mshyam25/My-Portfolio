f86d09c46adaba5bc4cf99862a9cb91b1d8a9815
mshyam25 <shyammanikandan.22@gmail.com>
2022-05-26 17:12:38 +0530

    Added Intro section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './Components/Header/Header'
+import IntroSection from './Components/Intro-Section/IntroSection'
 function App() {
   return (
     <div className='main'>
       <Header />
+      <IntroSection />
       <h1>Portfolio</h1>
     </div>
   )
